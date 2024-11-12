9a23cb0fb4ae5f1996c5d08c1a69ebc3fbc1f05d
Praveen Kumar Pothapakula <ppothapa@todi-ln001.cscs.ch>
2024-11-12 11:48:02 +0100

    changes for cloning

--- todi_install_dependencies.sh
+++ todi_install_dependencies.sh
@@ -37,13 +37,13 @@ done
 
 # Clone with specific branches
 
-#git clone -b $icon_branch git@github.com:C2SM/icon-exclaim.git
-#cp -r todi_nospack.dsl.nvidia_PPK icon-exclaim/config/cscs/ 
-#cp -r todi_env.sh icon-exclaim/config/cscs/
-#git clone -b $icon4py_branch git@github.com:C2SM/icon4py.git
+git clone -b $icon_branch git@github.com:C2SM/icon-exclaim.git
+cp -r todi_nospack.dsl.nvidia_PPK icon-exclaim/config/cscs/ 
+cp -r todi_env.sh icon-exclaim/config/cscs/
+git clone -b $icon4py_branch git@github.com:C2SM/icon4py.git
 #cp -r base-requirements.txt icon4py  
-#git clone -b $gt4py_branch https://github.com/GridTools/gt4py.git
-#git clone -b $gridtools_branch https://github.com/GridTools/gridtools.git
+git clone -b $gt4py_branch https://github.com/GridTools/gt4py.git
+git clone -b $gridtools_branch https://github.com/GridTools/gridtools.git
 
 
 
