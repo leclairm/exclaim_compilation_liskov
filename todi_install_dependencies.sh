6653106edd9de99b47f2136a9f404965603b74ba
Praveen Kumar Pothapakula <ppothapa@todi-ln003.cscs.ch>
2024-11-13 16:14:08 +0100

    todi install dependencies

--- todi_install_dependencies.sh
+++ todi_install_dependencies.sh
@@ -8,8 +8,8 @@ source ./todi_env.sh
 
 # Default branches if not provided
 icon_branch="icon-dsl"
-icon4py_branch="v0.0.13"
-gt4py_branch="v1.0.4"
+icon4py_branch="v0.0.14"
+gt4py_branch="icon4py_20241113"
 gridtools_branch="v2.3.7"
 
 
