e705dbe82cbf7393f463f205e290c2ce423a8f12
Matthieu <matthieu.leclair@c2sm.ethz.ch>
2025-04-09 10:26:33 +0200

    fix: remove "todi" here as well

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -l 
 set -e -x
 
-
 # Default branches if not provided
 icon_branch="reverse_advection"
 icon4py_branch="v0.0.14"
 gt4py_branch="icon4py_20241113"
 gridtools_branch="v2.3.7"
-
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -29,12 +27,9 @@ while [ "$1" != "" ]; do
     shift
 done
 
-
-
 # Clone with specific branches
-
 git clone --depth 1 --recurse-submodules --shallow-submodules -b $icon_branch git@github.com:C2SM/icon-exclaim.git
-cp -r todi_nospack.dsl.nvidia_PPK icon-exclaim/config/cscs/
+cp -r nospack.dsl.nvidia_PPK icon-exclaim/config/cscs/
 git clone --depth 1 -b $icon4py_branch git@github.com:C2SM/icon4py.git
 cp -r base-requirements.txt icon4py  
 cp -r requirements.txt icon4py
